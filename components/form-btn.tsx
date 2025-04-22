c468b684847704505455685b7fb6e2f94d22105b
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2025-04-23 01:32:32 +0900

    과제요건 충족하기 위해 버튼의 action스타일 추가

--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -10,7 +10,7 @@ export default function FormButton({ text }: FormButtonProps) {
   return (
     <button
       disabled={pending}
-      className='primary-btn h-10 disabled:bg-neutral-400 rounded-full disabled:text-neutral-300 disabled:cursor-not-allowed'
+      className='primary-btn h-10 disabled:bg-neutral-400 rounded-full disabled:text-neutral-300 disabled:cursor-not-allowed '
     >
       {pending ? '로딩 중' : text}
     </button>
